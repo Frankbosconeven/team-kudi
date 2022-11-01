a2ac82e06c31b2c89b78f31162524c75d068679a
Albert Nutifafa Dovlo <albert.dovlo@amalitech.org>
2022-11-01 06:21:45 +0000

    update

--- pages/components/homepage/homepage.jsx
+++ pages/components/homepage/homepage.jsx
@@ -1,73 +1,45 @@
-import React from 'react'
-import NavBar from '../navbar'
-import { Navigation, Pagination, Autoplay } from 'swiper';
+import React from "react";
+import NavBar from "../navbar";
+import { Navigation, Pagination, Autoplay } from "swiper";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "swiper/css/autoplay";
-import Footter from './footter';
-import CardBox from './cardbox';
-import Solution from './solution';
-import Team from '../team';
-import Ui from '../ui';
-
+import Footter from "./footter";
+import CardBox from "./cardbox";
+import Solution from "./solution";
+import Team from "../team";
+import Ui from "../ui";
 
 const Homepage = () => {
   return (
     <div>
       <NavBar />
-      <div className="w-screen h-72 bg-slate-500" >
-      <Swiper
-      className=''
-      modules={[Navigation, Pagination, Autoplay]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      autoplay={true}
-      pagination={{ clickable: true }}
-    >
-      <SwiperSlide>
-        <div className='block p-6 max-w-sm rounded-lg border border-gray-500 shadow-md'>
-          <h1>brief intro</h1>
+      <div className="w-screen h-72 bg-slate-200 flex justify-between m-auto ">
+        <div className="flex justify-between m-auto ">
+          <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white ">
+            Noteworthy technology acquisitions 2021
+          </h5>
         </div>
-      </SwiperSlide>
-        <SwiperSlide>
-          <div className='block p-6 max-w-sm rounded-lg border border-gray-500 shadow-md'>
-            <h1>what do</h1>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='block p-6 max-w-sm rounded-lg border border-gray-500 shadow-md'>
-            <h1>Lorem ipsum dolor sit.</h1>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='block p-6 max-w-sm rounded-lg border border-gray-500 shadow-md'>
-          <h1>Lorem ipsum dolor sit.</h1>
-          </div>
-        </SwiperSlide>
-
-    </Swiper>
       </div>
       <CardBox />
-    <div>
-    </div>
+      <div></div>
       <Solution />
-    <div>
       <div>
-        <Ui />
+        <div>
+          <Ui />
+        </div>
+        <div>
+          <Team />
+        </div>
+        <Footter />
       </div>
-      <div>
-        <Team/>
-      </div>
-      <Footter />
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
