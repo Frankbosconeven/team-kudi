8b6f62ad4602c0ae223e255b81c500aa7bfcff04
Albert Nutifafa Dovlo <albert.dovlo@amalitech.org>
2022-11-02 04:21:08 +0000

    updated problem explanation

--- pages/components/homepage/cardbox.jsx
+++ pages/components/homepage/cardbox.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function CardBox() {
   return (
     <>
-      <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-center mt-10">
+      <h5 class="mb-5 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white flex justify-center mt-10">
         Problem
       </h5>
       <div className="flex justify-evenly m-4 gap-8 flex-col lg:flex-row">
-        <div class="md:w-[50%] flex text-center items-center bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div class="md:w-[30%] flex justify-center text-center items-center bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           {/* <h1 className="items-center text-center">Problem3</h1> */}
           <a href="#">
             <img
@@ -18,17 +18,17 @@ export default function CardBox() {
           </a>
           <div class="p-5">
             <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <h5 class="mb-2 text-2xl font-serif font-bold tracking-tight text-gray-900 dark:text-white">
                 Lack of access to data and information
               </h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p class="mb-3 font-normal italic font-serif text-gray-700 dark:text-gray-400">
               Farmers lack access to information on good agricultural practices
             </p>
           </div>
         </div>
 
-        <div class="md:w-[50%] h-[300px] flex text-center items-center bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div class="md:w-[30%] h-[300px] flex text-center items-center bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           {/* <h1 className="items-center text-center">Problem3</h1> */}
           <a href="#">
             <img
@@ -39,11 +39,11 @@ export default function CardBox() {
           </a>
           <div class="p-5">
             <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <h5 class="mb-2 text-2xl font-bold font-serif tracking-tight text-gray-900 dark:text-white">
                 Lack access to Finance
               </h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p class="mb-3 font-normal italic font-serif text-gray-700 dark:text-gray-400">
               Out of the total formaing population ,70% are small holder farmers but these farmers lack access to finance to increase their production
             </p>
           </div>
--- pages/components/homepage/solution.jsx
+++ pages/components/homepage/solution.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Solution = () => {
   return (
     <>
-      <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-center mt-10">
+      <h5 class="mb-5 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white flex justify-center mt-10">
         Solution
       </h5>
       <div className="flex justify-center items-center text-center m-4">
-        <div class="md:w-[50%] h-[300px] flex text-center items-center bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div class="md:w-[40%] h-[300px] flex text-center items-center bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           {/* <h1 className="items-center text-center">Problem3</h1> */}
           <a href="#">
             <img
@@ -18,20 +18,20 @@ const Solution = () => {
           </a>
           <div class="p-[20px] md:p-[30%]  justify-center items-center text-center flex flex-col">
             <a href="#">
-              <h5 class="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <h5 class="mb-2 text-center text-2xl font-serif   font-bold tracking-tight text-gray-900 dark:text-white">
                 Data Driven Decision Making Model
               </h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p class="mb-3 font-normal text-gray-700 italic font-serif dark:text-gray-400">
               Data-Driven DecisionMaking
             </p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p class="mb-3 font-normal text-gray-700 italic font-serif dark:text-gray-400">
               Credit Scoring Algorithm
             </p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p class="mb-3 font-normal text-gray-700 italic font-serif dark:text-gray-400">
               Increase Farmers access to Finance
             </p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p class="mb-3 font-normal text-gray-700 italic font-serif dark:text-gray-400">
               Increase Supply to Market
             </p>
           </div>
